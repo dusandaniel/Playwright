cb40c3638cf6659b01bf602b74301fe2aafe72f3
dusan <dusan.daniel@gmail.com>
2024-08-31 11:36:24 +0200

    Ci test

--- tests/assertions.spec.ts
+++ tests/assertions.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Assertions', async ({ page }) => {
   await page.goto('http://www.saucedemo.com');
-  expect(page.locator('#user-name')).toContain
+  expect(page.locator('#user-name')).toBeVisible();
   await expect.soft(page.locator('#password')).toBeEditable();
   await expect(page.locator('#login-button')).toHaveCount(1);
   await expect(page.locator('#skillmea')).not.toBeVisible();
